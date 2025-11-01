2e0de7c9945d861454aeca428ec85c741015af0c
RaihanNaim <imraihan.naim@gmail.com>
2025-11-01 20:37:05 +0600

    first commit

--- index.js
+++ index.js
@@ -25,9 +25,20 @@ async function run() {
 
     const db = client.db("smart_db");
     const productsCollection = db.collection("products");
+    const bidsCollection = db.collection("bids");
 
     app.get("/products", async (req, res) => {
-      const cursor = productsCollection.find();
+      //     const projectsFields = { title: 1, price_min: 1, price_max: 1, image: 1}
+      //   const cursor = productsCollection.find().sort({price_min: 1}).skip(1).limit(5).project(projectsFields);
+
+      console.log(req.query);
+      const email = req.query.email;
+      const query = {};
+      if (email) {
+        query.email = email;
+      }
+
+      const cursor = productsCollection.find(query);
       const result = await cursor.toArray();
       res.send(result);
     });
@@ -68,6 +79,38 @@ async function run() {
       const result = await productsCollection.deleteOne(query);
       res.send(result);
     });
+
+    // bids related apis
+    app.get("/bids", async (req, res) => {
+      const email = req.query.email;
+      const query = {};
+      if (email) {
+        query.buyer_email = email;
+      }
+      const cursor = bidsCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.post("/bids", async (req, res) => {
+      const newBid = req.body;
+      const result = await bidsCollection.insertOne(newBid);
+      res.send(result);
+    });
+
+    app.delete("/bids/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await bidsCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    app.get('/bids/:id', async(req, res) =>{
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id)}
+        const result = bidsCollection.findOne(query);
+        res.send(result);
+    })
 
     await client.db("admin").command({ ping: 1 });
     console.log(
