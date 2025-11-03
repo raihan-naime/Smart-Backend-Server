aaaf61273c845ae3139b0f4d436e4cf8feccb4c4
RaihanNaim <imraihan.naim@gmail.com>
2025-11-03 21:46:26 +0600

    added env process

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 3000;
@@ -8,8 +9,9 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
+
 const uri =
-  "mongodb+srv://smartDbUser:YWRBPirEZhslisAj@cluster0.akcxiur.mongodb.net/?appName=Cluster0";
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.akcxiur.mongodb.net/?appName=Cluster0`;
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -133,7 +135,7 @@ async function run() {
         query.buyer_email = email
       }
       const cursor = bidsCollection.find(query);
-      const result = cursor.toArray();
+      const result = await cursor.toArray();
       res.send(result);
     })
 
@@ -154,9 +156,16 @@ async function run() {
     app.get("/bids/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
-      const result = bidsCollection.findOne(query);
+      const result = await bidsCollection.findOne(query);
       res.send(result);
     });
+
+    app.delete('/bids/:id', async(req, res) =>{
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id)};
+      const result = await bidsCollection.deleteOne(query);
+      res.send(result);
+    })
 
     await client.db("admin").command({ ping: 1 });
     console.log(
